aead22fbe1c641714f20b1bb0c124a69e38cf839
christopher-lee <clee651@terpmail.umd.edu>
2024-12-30 13:11:25 -0500

    Handle requests for a specific project page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 // Create the app and specify the port
 const app = express();
@@ -6,6 +7,9 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware for static files
 app.use(express.static("public"));
+
+// Middleware for parsing requests
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render("index.ejs");
@@ -19,8 +23,11 @@ app.get('/projects', (req, res) => {
     res.render("projects.ejs");
 })
 
-app.get('project-page', (req, res) => {
-    res.render('project-page.ejs');
+app.get('/project-page/:project', (req, res) => {
+    let project = req.params.project;
+    res.render('project-page.ejs', {
+        project: project,
+    });
 })
 
 app.get('/chalkboard', (req, res) => {
