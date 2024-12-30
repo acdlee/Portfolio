2655192fd65dbf1ee3fca329c500cb89ef0e0292
christopher-lee <clee651@terpmail.umd.edu>
2024-12-30 15:38:51 -0500

    Add default routing. Update styles. Add routing for all individual projects. Add test data for individual project page.

--- index.js
+++ index.js
@@ -11,34 +11,63 @@ app.use(express.static("public"));
 // Middleware for parsing requests
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Index page
 app.get('/', (req, res) => {
     res.render("index.ejs");
 })
 
+// About page
 app.get('/about', (req, res) => {
     res.render("about.ejs");
 })
 
+// Projects page
 app.get('/projects', (req, res) => {
     res.render("projects.ejs");
 })
 
+// Individual projects page
 app.get('/project-page/:project', (req, res) => {
     let project = req.params.project;
+    let targetProjectData = projectData[project];
     res.render('project-page.ejs', {
         project: project,
+        data: targetProjectData,
     });
 })
 
+// Chalkboard page
 app.get('/chalkboard', (req, res) => {
     res.render('chalkboard.ejs');
 })
 
+// Resume download route
 app.get('/resume', (req, res) => {
     const file = `${import.meta.dirname}/public/uploads/christopher_lee_resume.pdf`;
     res.download(file);
 })
 
+// Invalid route: default to index page
+app.get('*', (req, res) => {
+    res.redirect('/');
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
+
+const projectData = {
+    myCalendar: {
+        images: [
+            "/images/logo-javascript.svg", "/images/logo-javascript.svg",
+        ],
+        description: "Inspired from Google Calendar, manage your daily TODOs, jot down weekly notes, and get a heads-up on the week's weather using this React application.",
+        technologies: "React, Vite",
+        highlights: [
+            "Weather data fetching from weather.gov's API.",
+            "Advanced state management with custom useState hooks",
+            "Weekly notes and tasks stored locally",
+            "Custom useReducer hooks for weather-related payloads",
+        ],
+    },
+};
