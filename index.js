5a4fd7f0e52bdada0b97cfe06059901d3cd882a7
christopher-lee <clee651@terpmail.umd.edu>
2024-12-31 10:06:10 -0500

    Undo individual project page work. Move to Github to display/highlight projects.

--- index.js
+++ index.js
@@ -26,16 +26,6 @@ app.get('/projects', (req, res) => {
     res.render("projects.ejs");
 })
 
-// Individual projects page
-app.get('/project-page/:project', (req, res) => {
-    let project = req.params.project;
-    let targetProjectData = projectData[project];
-    res.render('project-page.ejs', {
-        project: project,
-        data: targetProjectData,
-    });
-})
-
 // Chalkboard page
 app.get('/chalkboard', (req, res) => {
     res.render('chalkboard.ejs');
@@ -55,19 +45,3 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
-
-const projectData = {
-    myCalendar: {
-        images: [
-            "/images/logo-javascript.svg", "/images/logo-javascript.svg",
-        ],
-        description: "Inspired from Google Calendar, manage your daily TODOs, jot down weekly notes, and get a heads-up on the week's weather using this React application.",
-        technologies: "React, Vite",
-        highlights: [
-            "Weather data fetching from weather.gov's API.",
-            "Advanced state management with custom useState hooks",
-            "Weekly notes and tasks stored locally",
-            "Custom useReducer hooks for weather-related payloads",
-        ],
-    },
-};
